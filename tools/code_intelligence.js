35ad4534c2853e97e340053c2496eedbb6166db0
randy888chan <57979441+randy888chan@users.noreply.github.com>
2025-07-29 16:58:25 +0700

    Update code_intelligence.js

--- tools/code_intelligence.js
+++ tools/code_intelligence.js
@@ -1,14 +1,10 @@
 import codeIntelligenceService from "../services/code_intelligence_service.js";
 
+// These are the currently implemented and functional tools.
 export async function findUsages({ symbolName }) {
   return codeIntelligenceService.findUsages({ symbolName });
 }
+
 export async function getDefinition({ symbolName }) {
   return codeIntelligenceService.getDefinition({ symbolName });
-}
-export async function getModuleDependencies({ filePath }) {
-  return ["/src/utils.js", "/src/config.js"];
-}
-export async function calculateCKMetrics({ className }) {
-  return { wmc: 15, dit: 3, noc: 2, cbo: 8, rfc: 25, lcom: 0.8 };
 }
