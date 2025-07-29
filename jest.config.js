20acf700afb8df33406d0731eb56ecbae3dfe174
randy888chan <57979441+randy888chan@users.noreply.github.com>
2025-07-29 16:20:24 +0700

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 export default {
   testEnvironment: "node",
-  transform: {
-    "^.+\\.js$": "babel-jest",
-  },
+  // No transform is needed for modern Node with ESM
   testMatch: ["**/tests/**/*.test.js"],
+  // This helps Jest understand it's an ESM project
+  extensionsToTreatAsEsm: [".js"],
+  moduleNameMapper: {
+    // This mapping helps with ESM imports if you have issues with extensions
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
 };
