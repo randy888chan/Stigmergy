aedb85ee1efb9e81b58dba365e710e5585813e33
randy888chan <57979441+randy888chan@users.noreply.github.com>
2025-09-25 10:47:07 +0700

    Create teardown.js

---
+++ tests/teardown.js
@@ -0,0 +1,9 @@
+import fs from 'fs-extra';
+import path from 'path';
+
+const TEST_TEMP_DIR = path.resolve(process.cwd(), '.stigmergy-core-test-temp');
+
+if (fs.existsSync(TEST_TEMP_DIR)) {
+  fs.removeSync(TEST_TEMP_DIR);
+  console.log(`[Global Teardown] Removed test directory: ${TEST_TEMP_DIR}`);
+}
